224f6a5e21fc0cb489634468853a566f8d79c237
Scapatici <ripiadinho@hotmail.com.br>
2021-06-28 17:27:43 -0300

    segundo_commit

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,18 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:payflow/modules/barcode_scanner/barcode_scanner_page.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/insert_boleto/insert_boleto_page.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 
 import 'modules/login/login_page.dart';
 
 class AppWidget extends StatelessWidget {
-  // This widget is the root of your application.
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Pay Flow',
         theme: ThemeData(
-          primaryColor: AppColors.primary,
-        ),
-        home: LoginPage());
+            primarySwatch: Colors.orange, primaryColor: AppColors.primary),
+        initialRoute: "/splash",
+        routes: {
+          "/splash": (context) => SplashPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage(),
+          "/barcode_scanner": (context) => BarcodeScannerPage(),
+          "/insert_boleto": (context) => InsertBoletoPage()
+        });
   }
 }
--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:payflow/modules/login/login_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -12,6 +14,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
@@ -55,9 +58,9 @@ class _LoginPageState extends State<LoginPage> {
                     padding:
                         const EdgeInsets.only(top: 40, left: 40, right: 40),
                     child: SocialLoginButton(onTap: () {
-                      print("clicou");
+                      controller.googleSignIn(context);
                     }),
-                  )
+                  ),
                 ],
               ),
             )
